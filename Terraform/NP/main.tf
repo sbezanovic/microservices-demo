37b0168a97944ee0feb68669890b4edaf0951cfe
sbezanovic <sbezanovic@griddynamics.com>
2024-05-27 16:15:04 +0200

    try diff zones

--- Terraform/NP/main.tf
+++ Terraform/NP/main.tf
@@ -1,13 +1,13 @@
 # Authenticate with GCP
 provider "google" {
   project     = "gd-gcp-gridu-devops-t1-t2"
-  region      = "us-central1"
+  region      = "europe-west1-b"
 }
 
 # Create a GKE cluster with zero initial nodes
 resource "google_container_cluster" "sbezanovic_cluster_np" {
   name     = "sbezanovic-cluster-np"
-  location = "us-central1"
+  location = "europe-west1-b"
   deletion_protection = false
   initial_node_count = 1
   #remove_default_node_pool = true
